b7d50cc102d8fc3a29ee60444368586cc7b586c4
emkataag <emkataag@abv.bg>
2022-05-27 21:17:39 +0300

    Remember search fields

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -4,7 +4,7 @@ const carService = require('../services/carService');
 router.get('/', (req, res )=>{
     let {mark , model , gen} = req.query;
     const cars = carService.getAll(mark, model, gen);
-    res.render('index' , {cars})
+    res.render('index' , {cars,mark,model, gen})
 });
 router.get('/about', (req,res)=>{
     res.render('about')
