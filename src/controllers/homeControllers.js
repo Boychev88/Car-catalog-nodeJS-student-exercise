7876a1f405e9f9e26092d32eb96a6805c8f535b3
emkataag <emkataag@abv.bg>
2022-05-27 21:12:05 +0300

    Add search

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,7 +1,9 @@
-const router = require('express').Router()
-const cars =require('../db.json')
+const router = require('express').Router();
+const carService = require('../services/carService');
 
 router.get('/', (req, res )=>{
+    let {mark , model , gen} = req.query;
+    const cars = carService.getAll(mark, model, gen);
     res.render('index' , {cars})
 });
 router.get('/about', (req,res)=>{
--- src/services/carService.js
+++ src/services/carService.js
@@ -3,16 +3,23 @@ const path = require('path');
 const cars = require('../db.json');
 const uniqId = require('uniqid')
 
+exports.getAll = (searchMark = '', searchModel = '', searchGen = '') => {
+    const result = cars 
+        .filter(x => x.mark.toLowerCase().includes(searchMark.toLowerCase()))
+        .filter(x => x.model.toLowerCase().includes(searchModel.toLowerCase()))
+        .filter(x => x.gen.toLowerCase().includes(searchGen.toLowerCase()));
+    return result;
 
-exports.getOne = (carId)=>{
- return cars.filter(x => x.id == carId)[0];
-} 
-    
+}
+
+exports.getOne = (carId) => {
+    return cars.filter(x => x.id == carId)[0];
+}
+
 
 exports.save = (car) => {
-    cars.push({id: uniqId(),...car});
+    cars.push({ id: uniqId(), ...car });
 
     let textData = JSON.stringify(cars, '', 4);
-    console.log(textData);
-    return fs.writeFile(path.resolve('src','db.json'),textData,{encoding: 'utf-8'})
+    return fs.writeFile(path.resolve('src', 'db.json'), textData, { encoding: 'utf-8' })
 }
